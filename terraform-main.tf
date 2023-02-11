cf2a1a650ab5bb6f550903652a62fa73f4c5ca62
mlt <miltozz@gmail.com>
2023-02-11 22:06:45 +0200

    upd terraform ec2 instance ansdok sg again

--- terraform-main.tf
+++ terraform-main.tf
@@ -106,10 +106,10 @@ resource "aws_security_group" "myapp-sg" {
   }
 
   ingress {
-    from_port   = 8081
+    from_port   = 8080
     to_port     = 8081
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.my_ip]
   }
 
   ingress {
