b85d3580d4f1837bd1de2d5589a81a4a604fdad3
mlt <miltozz@gmail.com>
2023-02-11 19:45:43 +0200

    updates

--- variables.tf
+++ variables.tf
@@ -13,37 +13,43 @@ variable "vpc_cidr_block" {
 variable "subnet_cidr_block" {
   description = "CIDR block for subnet"
   type        = string
-  default     = "10.0.1.0/24"
+  default     = "10.0.1.0/24"    
 }
 
 variable "avail_zone" {
   description = "az for subnet"
   type        = string
-  default     = "eu-central-1a"
+  default     = "eu-central-1a"     
 }
 
 variable "depl_env_prefix" {
   description = "Deployment environment prefix"
   type        = string
-  default     = "dev"
+  default     = "dev"     
 }
 
 variable "my_ip" {
   description = "My IP for SG SSH ingress"
   type        = string
-  default     = "87.202.58.227/32"
+  default     = "87.202.58.227/32"      
+}
+
+variable "ec2_ansible_server_ip" {
+  description = "SG SSH ingress for Ansible Server on an EC2 Instance, which runs the playbooks"
+  type        = string
+  default     = "15.188.50.237/32"      
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t2.micro"
+  default     = "t2.micro"     
 }
 
 variable "public_key_location" {
   description = "EC2 instance type"
   type        = string
-  default     = "C:\\Users\\MLT\\.ssh\\id_ed25519.pub"
+  default     = "C:\\Users\\MLT\\.ssh\\id_ed25519.pub"     
 }
 
 #####################################################################
@@ -63,7 +69,7 @@ variable "private_subnet_count" {
 variable "public_subnet_cidr_blocks" {
   description = "Available cidr blocks for public subnets."
   type        = list(string)
-  default = [
+  default     = [
     "10.0.1.0/24",
     "10.0.2.0/24",
     "10.0.3.0/24",
@@ -78,7 +84,7 @@ variable "public_subnet_cidr_blocks" {
 variable "private_subnet_cidr_blocks" {
   description = "Available cidr blocks for private subnets."
   type        = list(string)
-  default = [
+  default     = [
     "10.0.101.0/24",
     "10.0.102.0/24",
     "10.0.103.0/24",
