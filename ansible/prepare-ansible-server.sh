02eb734b1491e2d25d1dc0cd06b211c656bc7894
Miltos T <miltosdev@gmail.com>
2023-07-03 19:54:13 +0300

    trivial-updates

--- ansible/prepare-ansible-server.sh
+++ ansible/prepare-ansible-server.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-# when root
-# apt update
-# apt install ansible -y
-# apt install python3-pip -y
-# pip3 install boto3
-
 #ec2 ubuntu user
 sudo apt update
 sudo apt upgrade -y
